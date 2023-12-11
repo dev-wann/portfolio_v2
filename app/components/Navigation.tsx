fe638ad74b8f4d3417451e352703a02deca8f75e
seungwan_cho <swcho8220@gmail.com>
2023-12-11 00:41:16 -0800

    [feat] Add routes and styling to navigation bar
    - apply neumorphism style to nav bar
    - add routing buttons to nav bar & implemented css transitions for routing
        - temporarily added about, project, contact pages to test routing
    - refactor style files: css to scss
        - gather global style files to ‘/app/styles’ directory
        - add 'variables.scss' to nextConfig-prependData to use scss variables globally
        - refactored LangButton style

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,9 +1,41 @@
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import LangButton from './LangButton';
+import styles from './Navigation.module.scss';
+
+const routes = [
+  ['home', '/'],
+  ['about', '/about'],
+  ['project', '/project'],
+  ['contact', '/contact'],
+];
 
 export default function Navigation() {
+  const pathname = usePathname();
+  const selected = routes.findIndex((elem) => elem[1] === pathname);
+
+  const selectedBg: React.CSSProperties = {
+    marginLeft: `${(100 * selected) / routes.length}%`,
+    width: `${100 / routes.length}%`,
+  };
+
   return (
-    <nav>
-      <LangButton />
+    <nav className={`container-720 ${styles['nav-wrapper']}`}>
+      <div className={styles.navigation}>
+        <div className={styles.routes}>
+          <div className={styles['selected-bg']} style={selectedBg} />
+
+          {routes.map(([name, path], idx) => (
+            <Link
+              className={idx === selected ? styles.selected : ''}
+              href={path}
+            >
+              {name.toUpperCase()}
+            </Link>
+          ))}
+        </div>
+        <LangButton />
+      </div>
     </nav>
   );
 }
