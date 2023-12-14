036e2462123453064a2fc44b0df01b2eab0e7adb
seungwan_cho <swcho8220@gmail.com>
2023-12-14 11:15:30 -0800

    [chore] adjust colors
    - adjust light mode colors
    - fix some color-related errors

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import { RootState } from './redux';
 import strings from './strings.json';
 
 export default function Home() {
-  const lang = useSelector((state: RootState) => state.lang.data);
+  const lang = useSelector((state: RootState) => state.prefer.lang);
   if (!lang) return <></>;
 
   const str = strings[lang];
 
   return (
-    <main>
+    <main className="container-960">
       <p>{str[0]}</p>
       <p>{str[1]}</p>
     </main>
