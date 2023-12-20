2bc6a27ddc9f1757726a807873b6f54b8c11c85a
seungwan_cho <swcho8220@gmail.com>
2023-12-20 00:35:17 -0800

    [chore] organize style
    - global style
        - add new color: ‘—color-text-strong’ for better visibility of bold text
        - set rem as 18px
        - set default transitions
    - about page style
        - remove unnecessary neumorphic box from profile section
        - change grid style
        - add transition outset <-> inset transition style to details button
        - resolve ‘div in p’ warnings (related to arrows)
    - modified some strings

--- app/_components/about/Skill.tsx
+++ app/_components/about/Skill.tsx
@@ -2,7 +2,7 @@ import gStyles from './AboutGlobal.module.scss';
 
 export default function Skill() {
   return (
-    <div className={gStyles.card}>
+    <div className={gStyles.section}>
       <h1>Skill</h1>
     </div>
   );
