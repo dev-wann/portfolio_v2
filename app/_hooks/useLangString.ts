12fcfd88cf7fa4ec0b2a22f6184c23eef87693e7
seungwan_cho <swcho8220@gmail.com>
2023-12-19 21:57:43 -0800

    [feat] About page in progress: profile rough ver.
    - Roughly implement profile section of about page
        - add english ver. string (will be modified later)
        - add ‘Go to Resume’ & ‘Show Details’ button
            - TODO: implement each button’s functionalities
    - use Interweave library to convert json strings to html
        - to process bold tags in json strings
        - use renderText method(@ utils.tsx) in components
    - add global styles for h1, b, and p tags (globals.css)

--- app/_hooks/useLangString.ts
+++ app/_hooks/useLangString.ts
@@ -2,7 +2,9 @@ import strings from '@/public/strings.json';
 import { useSelector } from 'react-redux';
 import { RootState } from '../_redux';
 
-export default function useLangString(pageName: string) {
+export default function useLangString(pageName: string): {
+  [key: string]: string;
+} | null {
   const lang = useSelector((state: RootState) => state.prefer.lang);
   if (!lang) return null;
   return strings[lang][pageName];
