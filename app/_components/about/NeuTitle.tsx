90a70aebc6686e087f47061f1761ce4160a9b33f
seungwan_cho <swcho8220@gmail.com>
2023-12-21 15:49:03 -0800

    [chore] Style modification
    - change max width from 720 to 800 (about the size of A4 paper)
    - add some comment to globals.scss
    - Add '.' postfix to NeuTitle

--- app/_components/about/NeuTitle.tsx
+++ app/_components/about/NeuTitle.tsx
@@ -3,7 +3,7 @@ import styles from './NeuTitle.module.scss';
 export default function NeuTitle({ text }: { text: string }) {
   return (
     <div>
-      <span className={styles.text}>{text}</span>
+      <span className={styles.text}>{text + '.'}</span>
       <div className={styles.hairline}></div>
     </div>
   );
