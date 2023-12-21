59ed7a8555062ddc96f83968562a0cebc7143d89
seungwan_cho <swcho8220@gmail.com>
2023-12-20 19:32:12 -0800

    [feat] Implement NeuTitle component & apply font
    - NeuTitle.tsx
        - for section titles @ about page
        - consist of bold italic font and neumorphic hairline
    - others
        - use Pretenard font as default
        - change nav bar position to fixed & show page name when nav bar is small

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import ReduxProvider from './_components/ReduxProvider';
 import './_styles/globals.scss';
 import './_styles/reset.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: `Seungwan Cho's Portfolio`,
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ReduxProvider children={children} />
       </body>
     </html>
