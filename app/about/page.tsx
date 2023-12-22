3c5310b46df8f081a0ccd051ddfa5821e636cd64
seungwan_cho <swcho8220@gmail.com>
2023-12-22 02:02:43 -0800

    [feat] Roughly implemented Skills section
    - implemented bento style grid layout
    - implemented highlight & dehighlight on hover
    - added skill image files
    - TODO: need to add strings (both english and korean)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,14 @@
 import Experience from '../_components/about/Experience';
 import Profile from '../_components/about/Profile';
-import Skill from '../_components/about/Skill';
+import Skills from '../_components/about/Skills';
 import styles from './About.module.scss';
 
 export default function About() {
   return (
     <main className={styles.wrapper}>
       <Profile />
+      <Skills />
       <Experience />
-      <Skill />
     </main>
   );
 }
