c4df145df8aaa634fbc6e67a7b0ee6c1c833a7ec
seungwan_cho <swcho8220@gmail.com>
2023-12-27 02:24:15 -0800

    [feat] Implement left half of the contact page
    - Implement Links section of contact page, which is the left half of the page
    - add images needed
    - TODO: language change!

---
+++ app/_components/contact/DirectMessage.tsx
@@ -0,0 +1,9 @@
+import styles from './DirectMessage.module.scss';
+
+export default function DirectMessage() {
+  return (
+    <div className={styles.wrapper}>
+      <h2>Direct message</h2>
+    </div>
+  );
+}
