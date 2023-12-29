aa479d33b998d6115fee7450a7b0564ae1858a54
seungwan_cho <swcho8220@gmail.com>
2023-12-29 00:59:43 -0800

    [chore] clean up some css props
    - remove unnecessary css props

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,13 +5,12 @@ import Experience from '../_components/about/Experience';
 import Profile from '../_components/about/Profile';
 import Skills from '../_components/about/Skills';
 import useIntersectionObserver from '../_hooks/useIntersectionObserver';
-import styles from './About.module.scss';
 
 export default function About() {
   useIntersectionObserver();
 
   return (
-    <main className={styles.wrapper}>
+    <main>
       <Profile />
       <Skills />
       <Experience />
