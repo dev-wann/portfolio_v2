b3adc776af35f5ab0925f176b11de2e5f6491241
seungwan_cho <swcho8220@gmail.com>
2023-12-29 01:00:55 -0800

    [feat] Project page in progress
    - implemented sidebar
    - TODO: need to implemnt ProjectItem and fill valid data

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,3 +1,53 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import ProjectItem from '../_components/project/ProjectItem';
+import styles from './Project.module.scss';
+
+type Items = {
+  idx: number;
+}[];
+
 export default function Project() {
-  return <>Project page</>;
+  // useIntersectionObserver();
+  const [selected, setSelected] = useState(0);
+  useEffect(() => {
+    scrollTo(0, window.innerHeight / 2);
+  }, []);
+
+  const items = [{ idx: 1 }, { idx: 2 }, { idx: 3 }, { idx: 4 }];
+
+  // render
+  return (
+    <main className={styles.wrapper}>
+      <Sidebar items={items} selected={selected} setSelected={setSelected} />
+      {items.map((item) => (
+        <ProjectItem data={item} key={item.idx} />
+      ))}
+    </main>
+  );
+}
+
+function Sidebar({
+  items,
+  selected,
+  setSelected,
+}: {
+  items: Items;
+  selected: number;
+  setSelected: (input: number) => void;
+}) {
+  return (
+    <div className={styles['sidebar-wrapper']}>
+      <div className={styles.sidebar}>
+        {items.map((_, idx) => (
+          <button
+            key={idx}
+            className={idx === selected ? styles.selected : ''}
+            onClick={() => setSelected(idx)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
