bb5bc3a1730cc33b8f8e081e962d6e869e98af65
seungwan_cho <swcho8220@gmail.com>
2024-01-02 01:40:59 -0800

    [feat] Change how to save and play video
    - change opening and closing images to high quality ones
    - change main images to mp4 videos
      - found out that mp4 takes up the least memory
      - opening and closing are not replacable because of the fade in/out transparency effect
    - implement mp4 playing related logic to videoUtil.ts
    - implement interruption logic
      - use when theme is changed while playing video
      - when start playing video, generate a uuid
      - if theme changes before video end, push the id to stopRequest array
      - if id is registered in stopRequest, do not execute stage change

--- app/_components/home/BackgroundVideo.tsx
+++ app/_components/home/BackgroundVideo.tsx
@@ -2,9 +2,10 @@
 
 import { RootState } from '@/app/_redux';
 import {
-  VideoImagesType,
+  VideoImageType,
   getVideoImages,
   playVideo,
+  stopVideo,
 } from '@/app/_utils/videoUtil';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -13,15 +14,49 @@ import styles from './BackgroundVideo.module.scss';
 type Props = {};
 
 export default function BackgroundVideo({}: Props) {
-  const [stage, setStage] = useState<keyof VideoImagesType>('opening');
+  const [stage, setStage] = useState<keyof VideoImageType | 'ready' | 'finish'>(
+    'finish'
+  );
   const theme = useSelector((state: RootState) => state.prefer.theme);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  // id to interrupt video playing promise
+  const stopId = useRef<string | null>(null);
+  const stopRequest = useRef<string[]>([]);
+
+  // resetart when theme changed
   useEffect(() => {
-    if (!theme) return;
+    stopVideo();
+    if (stopId.current) stopRequest.current.push(stopId.current);
+    if (theme) setStage('ready');
+  }, [theme]);
+
+  // play closing before path or theme changes
+  useEffect(() => {
+    //TODO
+  });
+
+  // play video in order
+  useEffect(() => {
+    if (theme === null || stage === 'finish') return;
+    if (stage === 'ready') {
+      setStage('opening');
+      return;
+    }
     getVideoImages(theme).then(async (res) => {
       if (!canvasRef.current) return;
+      const newId = window.crypto.randomUUID();
+      stopId.current = newId;
       await playVideo(canvasRef.current, res[stage]);
+
+      // stop by interruption
+      const idx = stopRequest.current.indexOf(newId);
+      if (idx !== -1) {
+        stopRequest.current.splice(idx, 1);
+        return;
+      }
+
+      // set next stage
       switch (stage) {
         case 'opening':
           setStage('main');
@@ -29,15 +64,18 @@ export default function BackgroundVideo({}: Props) {
         case 'main':
           setStage('closing');
           break;
+        case 'closing':
+          setStage('finish');
         default:
           break;
       }
     });
-  }, [theme, stage]);
+  }, [stage]);
 
+  // render
   return (
     <div className={styles.wrapper}>
-      <canvas width={960} height={540} ref={canvasRef} />
+      <canvas width={1920} height={1080} ref={canvasRef} />
     </div>
   );
 }
--- app/_utils/videoUtil.ts
+++ app/_utils/videoUtil.ts
@@ -1,13 +1,13 @@
 import { THEME_ENUM } from '../_redux/module/preferSlice';
 
-export type VideoImagesType = {
+export type VideoImageType = {
   opening: HTMLImageElement[];
-  main: HTMLImageElement[];
+  main: HTMLVideoElement;
   closing: HTMLImageElement[];
 };
 
 // cache
-const cache: Record<string, VideoImagesType> = {};
+const cache: Record<string, VideoImageType> = {};
 
 // get video image data
 export async function getVideoImages(theme: keyof typeof THEME_ENUM) {
@@ -19,58 +19,99 @@ export async function getVideoImages(theme: keyof typeof THEME_ENUM) {
   return cache[theme];
 }
 
+let cancelId: number | null;
 // play video
 export function playVideo(
   canvas: HTMLCanvasElement,
-  images: HTMLImageElement[]
+  data: HTMLImageElement[] | HTMLVideoElement
 ) {
+  const context = canvas.getContext('2d');
+
+  // play video
+  if (data instanceof HTMLVideoElement) {
+    return new Promise((resolve) => {
+      let isPlaying = true;
+      data.addEventListener('ended', () => {
+        isPlaying = false;
+        resolve('');
+      });
+      data.play();
+      const play = () => {
+        context?.clearRect(0, 0, canvas.width, canvas.height);
+        context?.drawImage(data, 0, 0);
+        if (isPlaying) cancelId = requestAnimationFrame(play);
+      };
+      play();
+    });
+  }
+
+  // play images
   return new Promise((resolve) => {
-    const context = canvas.getContext('2d');
     let idx = 0;
     const play = () => {
-      if (idx === images.length) {
+      if (idx === data.length) {
         resolve('');
         return;
       }
-      context?.drawImage(images[idx], 0, 0);
+      context?.clearRect(0, 0, canvas.width, canvas.height);
+      context?.drawImage(data[idx], 0, 0);
       idx++;
-      requestAnimationFrame(play);
+      cancelId = requestAnimationFrame(play);
     };
     play();
   });
 }
 
+// stop video
+export function stopVideo() {
+  if (cancelId) cancelAnimationFrame(cancelId);
+  cancelId = null;
+}
+
 async function fetchVideoImages(theme: keyof typeof THEME_ENUM) {
-  const images: VideoImagesType = { opening: [], main: [], closing: [] };
+  const data: VideoImageType = {
+    opening: [],
+    main: document.createElement('video'),
+    closing: [],
+  };
   const promises: Promise<string>[] = [];
 
   // functions
-  const load = (idx: number, type: keyof VideoImagesType) => {
+  function load(type: keyof VideoImageType, idx?: number) {
     const promise = new Promise<string>((resolve) => {
-      const img = new Image();
-      img.src = pathMaker(idx, type);
-      img.addEventListener('load', () => resolve(''));
-      images[type].push(img);
+      let elem = type === 'main' ? data.main : new Image();
+      elem.src = pathMaker(type, idx);
+      if (type === 'main') {
+        (elem as HTMLVideoElement).muted = true;
+        elem.addEventListener('loadeddata', () => resolve(''));
+      } else if (elem instanceof HTMLImageElement) {
+        elem.addEventListener('load', () => resolve(''));
+        data[type].push(elem);
+      }
     });
     promises.push(promise);
-  };
-  const pathMaker = (idx: number, type: keyof VideoImagesType) => {
-    let name = String(idx);
-    name = '0'.repeat(4 - name.length) + name;
-    return `/images/home/typing-${type}-${theme}/${name}.webp`;
-  };
+  }
+  function pathMaker(type: keyof VideoImageType, idx?: number) {
+    let name = '';
+    if (type !== 'main') {
+      name = String(idx);
+      name = '/' + '0'.repeat(4 - name.length) + name;
+    }
+    return `/images/home/typing-${type}-${theme}${name}.${
+      type === 'main' ? 'mp4' : 'webp'
+    }`;
+  }
 
   // load data
+  load('main');
   if (theme === THEME_ENUM.DARK) {
-    for (let i = 0; i <= 340; i++) load(i, 'opening');
-    for (let i = 341; i <= 1121; i++) load(i, 'main');
-    for (let i = 1140; i <= 1300; i++) load(i, 'closing');
+    for (let i = 1; i <= 275; i++) load('opening', i);
+    for (let i = 1100; i <= 1250; i++) load('closing', i);
   } else {
-    for (let i = 40; i <= 340; i++) load(i, 'opening');
-    for (let i = 341; i <= 1171; i++) load(i, 'main');
-    for (let i = 1172; i <= 1312; i++) load(i, 'closing');
+    for (let i = 1; i <= 280; i++) load('opening', i);
+    for (let i = 1111; i <= 1215; i++) load('closing', i);
   }
 
   await Promise.all(promises);
-  return images;
+  return data;
 }
