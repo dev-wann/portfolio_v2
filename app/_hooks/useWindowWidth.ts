b1fe4de4f49ba1c2165c9bda61cb202d453878af
seungwan_cho <swcho8220@gmail.com>
2024-01-02 22:45:37 -0800

    [bugfix] window width related bugfix
    - use visualViewport.width instead of window.innerwidth if possible
    - resize nav bar not to fit 100vw
    - remove invalid width & height in contact wrapper
    - add meta viewport

--- app/_hooks/useWindowWidth.ts
+++ app/_hooks/useWindowWidth.ts
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 
 export default function useWindowWidth() {
   // null means not loaded yet (== useEffect is not excuted)
-  const [windowWidth, setWindowWidth] = useState<number | null>(null);
+  const [windowWidth, setWindowWidth] = useState<number>(
+    window.visualViewport?.width || window.innerHeight
+  );
 
   function changeWindowSize() {
-    setWindowWidth(window.innerWidth);
+    let width = window.visualViewport?.width || window.innerHeight;
+    setWindowWidth(width);
   }
 
   useEffect(() => {
