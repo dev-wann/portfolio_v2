9e2036a1ce1ad7b53dc74ee6a74019faa287467f
seungwan_cho <swcho8220@gmail.com>
2024-01-02 22:45:37 -0800

    [bugfix] window width related bugfix
    - use visualViewport.width instead of window.innerwidth if possible
    - resize nav bar not to fit 100vw
    - remove invalid width & height in contact wrapper
    - add meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Head from 'next/head';
 import ReduxProvider from './_components/ReduxProvider';
 import './_styles/globals.scss';
 import './_styles/reset.css';
@@ -15,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
       <body>
         <ReduxProvider children={children} />
       </body>
