66d817dbb6fe1a1d809496e2ae3579a65bd2ef42
seungwan_cho <swcho8220@gmail.com>
2024-01-17 17:00:29 +0900

    [feat] implement autoselect to experience section
    - autoselect works when none of the item is hovered
    - which item to select is determined by item position (tracking by scroll event)
    - make the period be highlighted by autoselect (others are highlighted only on hover)

--- app/_components/about/Experience.tsx
+++ app/_components/about/Experience.tsx
@@ -2,8 +2,9 @@
 
 import useCustomRouteTo from '@/app/_hooks/useCustomRouter';
 import useLangString from '@/app/_hooks/useLangString';
+import useWindowSize from '@/app/_hooks/useWindowSize';
 import { renderText } from '@/app/_utils';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NeuTitle from '../common/NeuTitle';
 import gStyles from './AboutGlobal.module.scss';
 import styles from './Experience.module.scss';
@@ -12,14 +13,61 @@ export default function Experience() {
   const strs = useLangString('about', 'experience');
   const routeTo = useCustomRouteTo();
 
+  // select0/select1 is set when item0/item1 is hovered
+  // autoSelect is set by the scroll event when nothing's hovered
   const [select0, setSelect0] = useState(false);
   const [select1, setSelect1] = useState(false);
-  const select = select0 ? styles.select0 : select1 ? styles.select1 : '';
+  const [autoSelect, setAutoSelect] = useState(-1);
 
+  const { windowHeight } = useWindowSize();
+  const item0Ref = useRef<HTMLDivElement>(null);
+
+  // scroll event for autoselect
+  useEffect(() => {
+    // event handler
+    const handleScroll = () => {
+      if (!item0Ref.current) return;
+      const top0 = item0Ref.current.getBoundingClientRect().top;
+
+      let sel = -1;
+      if (top0 < windowHeight * 0.7) sel = 0;
+      if (top0 < windowHeight * 0.5) sel = 1;
+      if (top0 < windowHeight * 0.3) sel = -1;
+      setAutoSelect(sel);
+    };
+
+    // event handler with throttle
+    let timeoutId: NodeJS.Timeout | null = null;
+    const handleScrollThrottle = () => {
+      if (timeoutId) return;
+      handleScroll();
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+      }, 100);
+    };
+
+    // add event listeners
+    handleScroll();
+    window.addEventListener('scroll', handleScrollThrottle);
+    window.addEventListener('scrollend', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScrollThrottle);
+      window.removeEventListener('scrollend', handleScroll);
+    };
+  }, [windowHeight]);
+
+  // choose select
+  let select = '';
+  if (select0) select = styles.select0;
+  else if (select1) select = styles.select1;
+  else if (autoSelect !== -1) select = styles[`select${autoSelect}`];
+
+  // render
   return (
     <section className={gStyles.section}>
       <NeuTitle text="Experience" className="observe" />
       <div className={gStyles.content} style={{ marginBottom: 0 }}>
+        {/* title */}
         <div className="observe text">
           <div className={styles.role}>
             <h2>{strs?.role || 'Software Engineer'}&ensp;&nbsp;</h2>
@@ -32,7 +80,9 @@ export default function Experience() {
             </h3>
           </div>
         </div>
+
         <div className={styles.grid}>
+          {/* slide */}
           <div className={`${styles.slide} observe`}>
             <div className={styles.bg} />
             <div className={`${styles.toggle} ${select}`} />
@@ -45,10 +95,13 @@ export default function Experience() {
               <span>—&ensp;&apos;19</span>
             </div>
           </div>
+
+          {/* item0 */}
           <div
             className={`${styles.project} observe text`}
             onMouseEnter={() => setSelect0(true)}
             onMouseLeave={() => setSelect0(false)}
+            ref={item0Ref}
           >
             <h3>
               SuperOffice {strs?.project || 'project'}
@@ -59,13 +112,19 @@ export default function Experience() {
                 DETAIL &gt;
               </button>
             </h3>
-            <p className={`${styles.period} ${select0 ? styles.select0 : ''}`}>
+            <p
+              className={`${styles.period} ${
+                select === styles.select0 ? styles.select0 : ''
+              }`}
+            >
               2021.02 ~ 2023.05
             </p>
             <p className={styles['project-desc']}>
               {strs?.superoffice || 'description'}
             </p>
           </div>
+
+          {/* item1 */}
           <div
             className={`${styles.project} observe text`}
             onMouseEnter={() => setSelect1(true)}
@@ -80,7 +139,11 @@ export default function Experience() {
                 DETAIL &gt;
               </button>
             </h3>
-            <p className={`${styles.period} ${select1 ? styles.select1 : ''}`}>
+            <p
+              className={`${styles.period} ${
+                select === styles.select1 ? styles.select1 : ''
+              }`}
+            >
               2019.08 ~ 2021.10
             </p>
             <p className={styles['project-desc']}>
